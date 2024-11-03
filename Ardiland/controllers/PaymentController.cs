ac8f7618341ecf7f33811c18ddf73c1406d3cc9b
ardalan ebrahimi <ardalan.ebrahimi@gmail.com>
2024-11-03 14:44:54 +0100

    add payment controller to use stripe

---
+++ Ardiland/controllers/PaymentController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Stripe;
+using System.Threading.Tasks;
+
+namespace Ardiland.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class PaymentController : ControllerBase
+{
+    private readonly IConfiguration _configuration;
+    public PaymentController(IConfiguration configuration)
+    {
+        StripeConfiguration.ApiKey = configuration["PaymentProviders:StripeApiKey"];
+    }
+
+    [HttpPost("create-payment-intent")]
+    public async Task<IActionResult> CreatePaymentIntent([FromBody] PaymentIntentRequest request)
+    {
+        var options = new PaymentIntentCreateOptions
+        {
+            Amount = request.Amount, // Amount in cents
+            Currency = "usd",
+            AutomaticPaymentMethods = new PaymentIntentAutomaticPaymentMethodsOptions { Enabled = true },
+        };
+
+        var service = new PaymentIntentService();
+        var paymentIntent = await service.CreateAsync(options);
+
+        return Ok(new { clientSecret = paymentIntent.ClientSecret });
+    }
+}
+
+public class PaymentIntentRequest
+{
+    public long Amount { get; set; }
+}
